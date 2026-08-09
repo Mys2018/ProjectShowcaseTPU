4b3524a9cc56e63106c0419d4cdebc9c2f07ebdb
1eshiy <1eshiy@vk.com>
2026-08-09 22:29:25 +0700

    add preferences store

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,12 +1,9 @@
-export { queryKeys as userKeys } from "./api/queryKeys";
-export { login, logout } from "./api/requests";
-export {
-  useAuthStatus,
-  useMe,
-  useUserById,
-} from "./api/queries";
-export { type AuthStatusResponse, type OAuthExchangeParams } from "./api/types";
-export { useAuthStore } from "./model/store/useAuthStore";
-export { type User, type UserRole } from "./model/types";
-export { getHighestRole } from "./lib/getHighestRole";
-export { placeholderUser, ROLES_TRANSLATIONS } from "./config/constants";
+export { queryKeys as userKeys } from './api/queryKeys'
+export { login, logout } from './api/requests'
+export { useAuthStatus, useMe, useUserById } from './api/queries'
+export { type AuthStatusResponse, type OAuthExchangeParams } from './api/types'
+export { useAuthStore } from './model/store/useAuthStore'
+export { usePreferencesStore } from './model/store/usePreferencesStore'
+export { type User, type UserRole } from './model/types'
+export * from './lib/roles'
+export { placeholderUser, ROLES_TRANSLATIONS } from './config/constants'
