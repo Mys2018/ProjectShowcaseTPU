0f80ddf514f127e2d73cd8305e9440e37e12e8d4
1eshiy <1eshiy@vk.com>
2026-08-20 21:23:44 +0700

    add project tags component

---
+++ src/entities/project/ui/project-tags/ProjectTags.tsx
@@ -0,0 +1,22 @@
+import styles from './ProjectTags.module.css'
+import clsx from 'clsx'
+import type { ProjectTag } from '../../model/types'
+
+interface ProjectTagsProps {
+  tags: ProjectTag[]
+  visibleCount?: number
+  className?: string
+}
+
+export function ProjectTags({ tags, visibleCount, className }: ProjectTagsProps) {
+  const visibleTags = tags.filter((_, i) => !visibleCount || i < visibleCount)
+  return (
+    <div className={clsx(styles.tags, className)}>
+      {visibleTags.map(tag => (
+        <div key={tag.tagId} className={styles.tag}>
+          {tag.tagName}
+        </div>
+      ))}
+    </div>
+  )
+}
