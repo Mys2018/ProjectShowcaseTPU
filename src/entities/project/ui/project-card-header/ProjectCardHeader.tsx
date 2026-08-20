3c486317203a6dd7ebdfadb12cf6d156b3b22571
1eshiy <1eshiy@vk.com>
2026-08-20 21:23:22 +0700

    add project card header component

---
+++ src/entities/project/ui/project-card-header/ProjectCardHeader.tsx
@@ -0,0 +1,22 @@
+import clsx from 'clsx'
+import type { ComponentPropsWithoutRef } from 'react'
+import styles from './ProjectCardHeader.module.css'
+import { getProjectTagBackground } from '../../lib/getProjectTagBackground'
+
+interface ProjectCardHeaderProps extends ComponentPropsWithoutRef<'div'> {
+  label: string
+  rotated?: boolean
+}
+
+export function ProjectCardHeader({ label, rotated, className, children, ...props }: ProjectCardHeaderProps) {
+  return (
+    <div
+      className={clsx(styles.header, rotated && styles.rotated, className)}
+      data-bg={label}
+      style={{ background: getProjectTagBackground(label) }}
+      {...props}
+    >
+      {children}
+    </div>
+  )
+}
