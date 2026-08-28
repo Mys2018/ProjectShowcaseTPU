66afe22927b10e482e2cda822740a34f7529f44a
1eshiy <1eshiy@vk.com>
2026-08-28 16:00:31 +0700

    add filled property to skeletons

--- src/shared/ui/skeleton/image-skeleton/ImageSkeleton.tsx
+++ src/shared/ui/skeleton/image-skeleton/ImageSkeleton.tsx
@@ -1,10 +1,12 @@
 import s1 from './ImageSkeleton.module.css'
 import s2 from '../Skeleton.module.css'
+import clsx from 'clsx'
 
 interface ImageSkeletonProps {
+  filled?: boolean
   className?: string
 }
 
-export function ImageSkeleton({ className }: ImageSkeletonProps) {
-  return <div className={`${s2.skeleton} ${s1.image} ${className ?? ''}`} />
+export function ImageSkeleton({ filled, className }: ImageSkeletonProps) {
+  return <div className={clsx(s2.skeleton, filled && s2.filled, s1.image, className)} />
 }
