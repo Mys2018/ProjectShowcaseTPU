528414668cd607f41b9e6c369a16729a4d482d68
1eshiy <1eshiy@vk.com>
2026-09-08 18:40:14 +0700

    add competency row skeleton

---
+++ src/entities/competency/ui/competency-row/CompetencyRowSkeleton.tsx
@@ -0,0 +1,16 @@
+import clsx from 'clsx'
+import styles from '../CompetencyRow.module.css'
+import { ImageSkeleton, TextSkeleton } from '@/shared'
+
+interface CompetencyRowSkeletonProps {
+  className?: string
+}
+
+export function CompetencyRowSkeleton({ className }: CompetencyRowSkeletonProps) {
+  return (
+    <div className={clsx(styles.competency, className)}>
+      <ImageSkeleton className={styles.icon} />
+      <TextSkeleton className={styles.label} />
+    </div>
+  )
+}
