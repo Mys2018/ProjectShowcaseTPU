a2ef28519d53a383030aa73dda69dee9c3c50a49
1eshiy <1eshiy@vk.com>
2026-09-09 23:47:25 +0700

    update date mapper

--- src/shared/lib/date/mappers.ts
+++ src/shared/lib/date/mappers.ts
@@ -1,12 +1,18 @@
-export const mapDateToLocalString = (date: Date, params?: { year?: boolean; time?: boolean }) =>
-  date
-    .toLocaleString('ru-RU', {
-      day: 'numeric',
-      month: 'long',
-      ...(params?.time && { hour: '2-digit', minute: '2-digit' }),
-      ...(params?.year && { year: 'numeric' })
-    })
-    .replace(' в ', ' в ')
+export const mapDateToLocalString = (date: Date, params?: { digitsOnly?: boolean; year?: boolean; time?: boolean }) =>
+  params?.digitsOnly
+    ? date.toLocaleDateString('ru-RU', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+      })
+    : date
+        .toLocaleString('ru-RU', {
+          day: 'numeric',
+          month: 'long',
+          ...(params?.time && { hour: '2-digit', minute: '2-digit' }),
+          ...(params?.year && { year: 'numeric' })
+        })
+        .replace(' в ', ' в ')
 
 export const mapDateToBackendString = (date: Date) => {
   const day = date.getDate().toString().padStart(2, '0')
