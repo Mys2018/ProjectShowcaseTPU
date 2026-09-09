80da50602d6331b5c562d215eebb692a295f3b64
1eshiy <1eshiy@vk.com>
2026-09-09 19:35:40 +0700

    rework project public status label

--- src/entities/project/lib/translations.ts
+++ src/entities/project/lib/translations.ts
@@ -1,4 +1,5 @@
-import type { ProjectFormat } from '../model/types'
+import type { ProjectFormat, ProjectStatus } from '../model/types'
+import { assertNever } from '@/shared'
 
 export const getProjectFormatTranslation = (format: ProjectFormat) => {
   switch (format) {
@@ -8,5 +9,32 @@ export const getProjectFormatTranslation = (format: ProjectFormat) => {
       return 'Учебный'
     case 'Real':
       return 'Реальный'
+  }
+}
+
+export const getProjectStatusTranslation = (status: ProjectStatus) => {
+  switch (status) {
+    case 'Active':
+      return 'Активен'
+    case 'Approved':
+      return 'Утверждён'
+    case 'Completed':
+      return 'Завершён'
+    case 'InProgress':
+      return 'В работе'
+    case 'NeedsRework':
+      return 'Требует доработки'
+    case 'NotImplemented':
+      return 'Не реализован'
+    case 'Pending':
+      return 'На модерации'
+    case 'Recruiting':
+      return 'Набор на проект'
+    case 'RecruitmentCompleted':
+      return 'Набор завершён'
+    case 'Rejected':
+      return 'Отклонён модератором'
+    default:
+      return assertNever(status)
   }
 }
