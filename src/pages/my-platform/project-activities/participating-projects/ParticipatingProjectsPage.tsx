48ac25372a1d93045142b9c6973e087cc8ff0408
1eshiy <1eshiy@vk.com>
2026-09-10 16:37:22 +0700

    show grade instead of year in participating projects page

--- src/pages/my-platform/project-activities/participating-projects/ParticipatingProjectsPage.tsx
+++ src/pages/my-platform/project-activities/participating-projects/ParticipatingProjectsPage.tsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx'
 import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 import styles from './ParticipatingProjectsPage.module.css'
 import { getFilteredProjects } from './lib/getFIlteredProjects'
 import { StudentParticipatingProjectCard } from '@/widgets/student-project-card'
@@ -8,9 +7,6 @@ import { useMe } from '@/entities/user'
 import { NoProjectsFallback, useParticipatingProjects } from '@/entities/project'
 
 export function ParticipatingProjectsPage() {
-  const navigate = useNavigate()
-  const thisYear = new Date().getFullYear()
-
   const { data } = useParticipatingProjects()
   const projects = data?.projects || []
 
@@ -50,12 +46,12 @@ export function ParticipatingProjectsPage() {
           <div className={styles.buttonList}>
             {Array.from({ length: myGrade }, (_, i) => (
               <button
-                className={clsx(styles.gradeButton, myGrade - i === selectedArchiveGrade && styles.active)}
-                onClick={() => setSelectedArchiveGrade(myGrade - i)}
+                className={clsx(styles.gradeButton, i + 1 === selectedArchiveGrade && styles.active)}
+                onClick={() => setSelectedArchiveGrade(i + 1)}
                 type='button'
-                key={i}
+                key={i + 1}
               >
-                {myGrade - i} курс
+                {i + 1} курс
               </button>
             ))}
           </div>
@@ -67,7 +63,7 @@ export function ParticipatingProjectsPage() {
             ))
           ) : (
             <div className={styles.description}>
-              <h5 className={styles.heading}>В {thisYear} году вы не участвовали в проектах</h5>
+              <h5 className={styles.heading}>На {selectedArchiveGrade} курсе вы не участвовали в проектах</h5>
               <p className={styles.paragraph}>
                 Выбирайте подходящие проекты из каталога. Все проекты, в которых вы участвовали, будут храниться здесь вместе со
                 статистикой.
