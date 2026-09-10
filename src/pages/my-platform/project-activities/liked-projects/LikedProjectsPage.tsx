a337f68c3c258eb968c4bb6e9f0836f875a436b7
1eshiy <1eshiy@vk.com>
2026-09-10 16:38:40 +0700

    add custom fallback to liked projects page

--- src/pages/my-platform/project-activities/liked-projects/LikedProjectsPage.tsx
+++ src/pages/my-platform/project-activities/liked-projects/LikedProjectsPage.tsx
@@ -1,11 +1,20 @@
 import styles from './LikedProjectsPage.module.css'
 import { ProjectsGrid } from '@/widgets/projects-grid'
+import { NoProjectsFallback } from '@/entities/project'
 
 export function LikedProjectsPage() {
   return (
     <div className={styles.container}>
       <h3 className={styles.title}>Понравившиеся проекты</h3>
-      <ProjectsGrid type='liked' />
+      <ProjectsGrid
+        type='liked'
+        emptyFallback={
+          <NoProjectsFallback
+            title='Понравившихся проектов пока нет'
+            description='Переходите в каталог проектов, выбирайте интересующие и успевайте подать заявки до конца набора!'
+          />
+        }
+      />
     </div>
   )
 }
