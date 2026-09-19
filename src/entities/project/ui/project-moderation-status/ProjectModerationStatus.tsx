7c8ec300c832cef0dea93e78261b17ed54fc6086
1eshiy <1eshiy@vk.com>
2026-09-20 00:23:36 +0700

    make project moderation status better

--- src/entities/project/ui/project-moderation-status/ProjectModerationStatus.tsx
+++ src/entities/project/ui/project-moderation-status/ProjectModerationStatus.tsx
@@ -1,6 +1,8 @@
 import styles from './ProjectModerationStatus.module.css'
 import type { ProjectStatus } from '../../model/types'
 import clsx from 'clsx'
+import { getModerationStatusIcon } from '../project-icons/ProjectIcons'
+import { getModerationStatusTranslation } from '../../lib/translations'
 
 interface ProjectModerationStatusProps {
   status: ProjectStatus
@@ -8,5 +10,8 @@ interface ProjectModerationStatusProps {
 }
 
 export function ProjectModerationStatus({ status, className }: ProjectModerationStatusProps) {
-  return <div className={clsx(styles.status, styles[status.toLowerCase()], className)}></div>
+  return <div className={clsx(styles.status, styles[status], className)}>
+    {getModerationStatusIcon(status)}
+    <p className={styles.label}>{getModerationStatusTranslation(status)}</p>
+  </div>
 }
