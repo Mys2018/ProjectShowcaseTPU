5896c77ba67ee4de94336c18746961314f34e030
1eshiy <1eshiy@vk.com>
2026-09-20 12:06:53 +0700

    fix: project format hardcoding

--- src/widgets/project-moderation/ProjectModeration.tsx
+++ src/widgets/project-moderation/ProjectModeration.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx'
 import styles from './ProjectModeration.module.css'
 import { ApproveProjectButton, RejectProjectButton, RequestChangesProjectButton } from '@/features/moderate-projects'
-import { ProjectInfo, ProjectReviewComment, useProjectReview, type ProjectCardData } from '@/entities/project'
+import { getProjectFormatTranslation, ProjectInfo, ProjectReviewComment, useProjectReview, type ProjectCardData } from '@/entities/project'
 import { ProjectSkeleton } from '@/shared'
 
 interface ProjectModerationProps {
@@ -19,7 +19,7 @@ export function ProjectModeration({ project }: ProjectModerationProps) {
               Проверка проекта «{project.meta.title}»
             </h2>
             <h3 className={styles.subtitle}>
-              Тип проекта: <span className={styles.regular}>Учебный</span>
+              Тип проекта: <span className={styles.regular}>{getProjectFormatTranslation(project.type)}</span>
             </h3>
           </div>
           {isReviewLoading ? <ProjectSkeleton /> : review && <ProjectReviewComment comment={review} />}
